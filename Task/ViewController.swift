69e18911e1d8621a22120420340d6d28641f35c6
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2018-11-22 11:07:32 +0900

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by 玉城翔平 on 2018/11/22.
+//  Copyright © 2018 玉城翔平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
